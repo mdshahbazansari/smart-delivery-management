9135d4580d0a2115c1801439319c5e74f42e4a38
MD SHAHBAZ ANSARI <MDSHAHBAZ8298@GMAIL.COM>
2025-03-13 12:52:05 +0530

    new

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,13 +1,21 @@
-import { connectDB } from '@/lib/db'
-import Order from '@/app/api/models/order'
-import { NextResponse } from 'next/server'
+import { connectDB } from '@/lib/db';
+import Order from '@/app/api/models/order';
+import { NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 
-export async function PUT(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
-  await connectDB()
-  const data = await req.json()
-  const order = await Order.findByIdAndUpdate(params.id, data, { new: true })
-  return NextResponse.json({ message: 'Order Update Successfully', order })
+export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+  await connectDB();
+  const data = await req.json();
+
+  if (!context.params.id) {
+    return NextResponse.json({ error: 'Order ID is required' }, { status: 400 });
+  }
+
+  const order = await Order.findByIdAndUpdate(context.params.id, data, { new: true });
+
+  if (!order) {
+    return NextResponse.json({ error: 'Order not found' }, { status: 404 });
+  }
+
+  return NextResponse.json({ message: 'Order Updated Successfully', order });
 }
